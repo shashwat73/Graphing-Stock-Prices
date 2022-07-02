5204c9f26268c669a320238f13ea7b355b8d2046
Shashwat Sharma <68554481+shashwat73@users.noreply.github.com>
2022-07-02 12:25:23 -0400

    Update main.R
    updating dates

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 install.packages("quantmod")
 library("quantmod")
 
-getSymbols("NFLX", from = "2022-1-1", to = "2022-4-28", auto.assign = TRUE)
+getSymbols("NFLX", from = "2022-1-1", to = "2022-7-1", auto.assign = TRUE)
 
 closingprice <- NFLX$NFLX.Close
 ts.plot(closingprice)
